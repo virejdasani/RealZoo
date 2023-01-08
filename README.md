dcc6b2d535e72490f3eab00892d0ef894447bb7b
Virej Dasani <virej@Virejs-MBP.broadband>
2023-01-08 18:15:38 +0000

    sUbScRiBe To ViReJ

---
+++ README.md
@@ -0,0 +1,1 @@
+I know I just put images everywhere but i made this in 20 minutes leave me alone
--- confetti.js
+++ confetti.js
@@ -1,3 +1,5 @@
+// I know I just put images everywhere but i made this in 20 minutes leave me alone
+// also i stole this from codepen, thanks
 "use strict";
 
 window.onload = function () {
